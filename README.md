52f501e46fda46e3c48e0c6829f5c34e4a998685
alvamau5 <lucioaalvaradomolina@outlook.com>
2023-06-11 14:34:50 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Weather App
+
+Aplicacion del clima consumiendo la API de OpenWeather, en la cual
+mediante busqueda por el nombre de ciudad y codigo del pais separado
+por una coma, que sera el campo de busqueda devolvera la solicitud
+mediante tarjetas. La unidad de temperatura utilizada en este caso
+sera Celsius.
+
+[![Captura-desde-2023-05-30-22-15-21.png](https://i.postimg.cc/N0zbdJXg/Captura-desde-2023-05-30-22-15-21.png)](https://postimg.cc/sB5pXJK8)
+
+[![Captura-desde-2023-05-30-22-28-44.png](https://i.postimg.cc/28X68MPd/Captura-desde-2023-05-30-22-28-44.png)](https://postimg.cc/zVK5xtD3)
+
+[![Captura-desde-2023-05-30-22-29-06.png](https://i.postimg.cc/DytTNT9N/Captura-desde-2023-05-30-22-29-06.png)](https://postimg.cc/kBFzVkXN)
+
+[![Captura-desde-2023-05-30-22-29-44.png](https://i.postimg.cc/L8pHXSx1/Captura-desde-2023-05-30-22-29-44.png)](https://postimg.cc/Wh5RWBLN)
+
+[![Captura-desde-2023-05-30-22-30-09.png](https://i.postimg.cc/ZnNSbs3H/Captura-desde-2023-05-30-22-30-09.png)](https://postimg.cc/1VyTMHvV)
+
+Lenguajes utilizados
+
+- JavaScript
+- CSS
+- HTML
---
+++ src/index.js
@@ -0,0 +1,94 @@
+/**
+ * Envio de peticicion de usario al formulario
+ */
+
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+// const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".tarjeta-section .cities");
+/* SUBSCRIBIRSE AQUI PARA OBETENER UNA API KEY: https://home.openweathermap.org/users/sign_up */
+const apikey = "f307a515194902e4f177a9cff13e799d";
+
+form.addEventListener("submit", search);
+
+// Verificar si esta vacio el buscador de pais
+function search(e) {
+  e.preventDefault();
+
+  const clima = document.querySelector(".top-banner form input").value;
+
+  if (clima === "") {
+    // console.log("Este campo es obligatorio...");
+
+    mostrarError("Por favor esribe una ciudad correcta...");
+
+    input.focus();
+    return;
+  }
+  // Consultar API
+  consultarAPI(clima);
+}
+
+// Creacion de alertas
+function mostrarError(error) {
+  const mostrarError = document.createElement("p");
+  mostrarError.textContent = error;
+
+  // Inyectando HTML
+  const contenido = document.querySelector(".top-banner form .msg");
+  contenido.appendChild(mostrarError);
+
+  // Eliminar automaticamente la alerta
+  setTimeout(() => {
+    mostrarError.remove();
+  }, 3000);
+
+  form.reset();
+  input.focus();
+}
+
+// Consumiendo API
+function consultarAPI(clima) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${clima}&appid=${apikey}&units=metric`;
+
+  fetch(url)
+    .then(respons => respons.json())
+    .then(data => {
+      // console.log(data)
+      if (data.cod === "404") {
+        mostrarError("Ciudad no encontrada...😩");
+
+        form.reset();
+        input.focus();
+        return;
+      }
+      mostrarClima(data);
+    });
+}
+
+// Inyectando HMTL para la creeacion de las tarjetas con la informacion del clima
+function mostrarClima(data) {
+  const { main, name, sys, weather } = data;
+  const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
+
+  // console.log(main, weather)
+
+  const li = document.createElement("li");
+  li.classList.add("city");
+  const tarjeta = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src="${icon}" alt="${weather[0]["description"]}">
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+      `;
+  li.innerHTML = tarjeta;
+  list.appendChild(li);
+
+  form.reset();
+  input.focus();
+}
